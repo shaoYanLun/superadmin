6aa9ac5b1a74d7c0061dc901d493037cbca523ca
dysir <1030302324@qq.com>
2017-10-19 17:30:04 +0800

    xx

--- application/views/layer/base_header.php
+++ application/views/layer/base_header.php
@@ -44,9 +44,12 @@
 			<ul class="nav navbar-nav pull-right">
 				<li class="dropdown dropdown-user">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-					<img alt="" class="img-circle" src="<?=static_url('pages/img/avatar3_small.jpg')?>"/>
+						<?php
+						$userinfo = checkLogin();
+						?>
+					<img alt="" class="img-circle" src="<?=static_url('global/img/dh.jpg')?>"/>
 					<span class="username username-hide-on-mobile">
-					Nick </span>
+					<?php echo empty($userinfo['nick_name'])?$userinfo['username']:$userinfo['nick_name'];?> </span>
 					<i class="fa fa-angle-down"></i>
 					</a>
 					<ul class="dropdown-menu dropdown-menu-default">
@@ -61,12 +64,8 @@
 						<li class="divider">
 						</li>
 						<li>
-							<a href="extra_lock.html">
-							<i class="icon-lock"></i> Lock Screen </a>
-						</li>
-						<li>
-							<a href="login.html">
-							<i class="icon-key"></i> Log Out </a>
+							<a href="/m/auth/logout">
+							<i class="icon-key"></i>登出 </a>
 						</li>
 					</ul>
 				</li>
